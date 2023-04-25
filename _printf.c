525541f2723312d9b447c91f8828714a18a47294
meriembenayad <meriem.benayad@gmail.com>
2023-04-25 17:27:31 +0000

    update _printf

--- _printf.c
+++ _printf.c
@@ -7,8 +7,11 @@
 int _printf(const char *format, ...)
 {
 	int count = 0, i, j, flag, space_flag = 0;
-	print_t print_funcs[] = {
-		{"c", print_char}, {"s", print_string}, {"%", print_percent}, {"d", print_int}, {"i", print_int}, {"b", convert_to_binary}, {"u", print_unsigned}, {"o", print_octal}, {"x", print_hexa_lower}, {"X", print_hexa_upper}, {"S", print_S}, {NULL, NULL}};
+	print_t print_funcs[] = {{"c", print_char}, {"s", print_string},
+		{"%", print_percent}, {"d", print_int}, {"i", print_int},
+		{"b", convert_to_binary}, {"u", print_unsigned}, {"o", print_octal},
+		{"x", print_hexa_lower}, {"X", print_hexa_upper}, {"S", print_S},
+		{NULL, NULL}};
 	va_list args;
 
 	va_start(args, format);
@@ -17,25 +20,19 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			while (format[i] == ' ')
-			{
+			for (; format[i] == ' '; i++)
 				space_flag = 1;
-				i++;
-			}
 			flag = 0;
 			for (j = 0; print_funcs[j].spec != NULL; j++)
 			{
-				if (format[i] == *print_funcs[j].spec)
+				if (space_flag && format[i] == *print_funcs[j].spec)
 				{
-					if (space_flag)
-					{
-						count += _putchar(' ');
-						space_flag = 0;
-					}
-					count += print_funcs[j].f(args);
-					flag = 1;
-					break;
+					count += _putchar(' ');
+					space_flag = 0;
 				}
+				count += print_funcs[j].f(args);
+				flag = 1;
+				break;
 			}
 			if (flag == 0)
 			{
